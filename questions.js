48a5b736a8eab3fdd8e959bf9cf843783334d4a3
Embedded Electronics <embeddedelectronics.in@gmail.com>
2025-08-13 16:10:58 +0530

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,22 @@
+const quizQuestions = [
+  {
+    question: "Which of the following microcontroller architectures is based on ARM Cortex-M?",
+    options: ["STM32", "PIC18F", "AT89C51", "8051"],
+    correct: 0
+  },
+  {
+    question: "Which protocol is used for communication between Arduino and PC via USB?",
+    options: ["SPI", "UART", "I2C", "CAN"],
+    correct: 1
+  },
+  {
+    question: "What does GPIO stand for?",
+    options: [
+      "General Purpose Input Output",
+      "General Programmed Internal Output",
+      "Graphical Processing Interface Option",
+      "Ground Pin Input Output"
+    ],
+    correct: 0
+  }
+];

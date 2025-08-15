9419b3b5daaa4aa5fbfefff771ff0f69de8cd402
Embedded Electronics <embeddedelectronics.in@gmail.com>
2025-08-15 15:36:44 +0530

    Update quizData.js

--- quizData.js
+++ quizData.js
@@ -1,24 +1,12 @@
 const quizData = {
   linux: [
     { question: "What command is used to list files?", options: ["ls", "dir", "list"], answer: "ls" },
-    { question: "What command is used to list files?", options: ["ls", "dir", "list"], answer: "ls" },
-    { question: "What command is used to list files?", options: ["ls", "dir", "list"], answer: "ls" },
-    { question: "What command is used to list files?", options: ["ls", "dir", "list"], answer: "ls" },
-    { question: "What command is used to list files?", options: ["ls", "dir", "list"], answer: "ls" },
-    { question: "What command is used to list files?", options: ["ls", "dir", "list"], answer: "ls" },
-    { question: "What command is used to list files?", options: ["ls", "dir", "list"], answer: "ls" },
-    { question: "What command is used to list files?", options: ["ls", "dir", "list"], answer: "ls" },
-    { question: "What command is used to list files?", options: ["ls", "dir", "list"], answer: "ls" },
     { question: "What is the root directory symbol?", options: ["/", "~", "."], answer: "/" }
   ],
   microcontrollers: [
     { question: "Which is a popular microcontroller?", options: ["Arduino UNO", "Raspberry Pi", "ESP8266"], answer: "Arduino UNO" }
   ],
   cprogramming: [
-{
-  question: "What will be the output of the following code?<br><pre>#include &lt;stdio.h&gt;<br>int main() {<br>    printf(\"%d\", 5 + 3);<br>    return 0;<br>}</pre>",
-  options: ["5", "8", "Error"],
-  answer: "8"
-}
+    { question: "Which header file is used for printf?", options: ["<stdio.h>", "<stdlib.h>", "<string.h>"], answer: "<stdio.h>" }
   ]
 };
